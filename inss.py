2f39cfe8186bd7a61be8dab2e32d2bdf63d4504e
Davicm1 <corrademedeirosdavi@gmail.com>
2024-10-10 20:56:18 -0300

    inss 2.0
    atualizado pra valores reais.

--- inss.py
+++ inss.py
@@ -22,6 +22,13 @@ dec1=dec1.lower()
 if dec1 == "s":
     dec2=int(input("deseja visualizar qual ordem de funcionário?:"))-1
 valorhr=salário/horas
-inss= (salário*9)/100
-total= inss-salário
-print("O funcionário: {}, com o id {},tem o salário: {}, por hora ganha: {}, de inss será pago {}, totalizando:".format(nlista[dec2],dec2, slista[dec2], valorhr, inss, total))
+if salário <= 1412:
+    inss=(salário*7.5)/100
+elif salário > 1412 and salário < 2666:
+    inss=(salário*9)/100
+elif salário > 2666 and salário < 4000:
+    inss=(salário*12)/100
+elif salário > 4000:
+    inss=(salário*14)/100
+total=salário-inss
+print("O funcionário: {}, com o id {},tem o salário: {}, por hora ganha: {}, de inss será pago {}, totalizando:".format(nlista[dec2],dec2+1, slista[dec2], valorhr, inss, total))
