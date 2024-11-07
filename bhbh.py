9df7d269b87bec88f21cfe0758b2b06befa7bae3
Davicm1 <corrademedeirosdavi@gmail.com>
2024-11-07 20:59:04 -0300

    exe mpls de if

---
+++ bhbh.py
@@ -0,0 +1,30 @@
+import numpy as np
+#b= np.array([[1, 2, 3], [4, 5, 6]])
+#print(b)
+#print(b[1])
+#print (max(b[0]))
+#print(sum(b[0]))
+print("========================================================================")
+#matriz_dez= np.arange(10)
+#print(matriz_dez)
+#print("==============")
+#pula_dois= np.arange(3, 10, 2)
+#print(pula_dois)
+
+#a= np.array([1, 2, 3])
+#print(a)
+#print(type(a))
+
+while True:
+   print("UP NÃO É ESPORTIVO!!!!")
+   inicio=int(input("digite um numero de 1 a 500"))
+   if inicio <=500:
+   breakpoint
+   final=int(input("digite um numero de 2 a 1000"))
+   elif final>= 1000:
+   breakpoint
+   pulo=int(input("digite um numero 1 a 50"))
+   elif pulo >= 50:
+   UP_DE_MERDA=np.arange(inicio,final,pulo)
+   print(UP_DE_MERDA)
+    break
